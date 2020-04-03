061d2bbf30a7726dcc67d6174cb65d09fb67ad8d
pladmin pladmin <pladmin@capgemini.com>
2020-04-03 20:59:16 +0200

    first test

---
+++ test.py
@@ -0,0 +1,8 @@
+from webbot import Browser 
+web = Browser()
+web.go_to('https://www.ubs.com/')
+web.click('UBS logins')
+web.type('mymail@gmail.com' , into='Email')
+web.click('NEXT' , tag='span')
+web.type('mypassword' , into='Password' , id='passwordFieldId') # specific selection
+web.click('NEXT' , tag='span') # you are logged in ^_^
